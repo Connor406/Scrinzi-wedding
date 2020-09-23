d33ddca88314b887115fb3687b1c0d36d0dac822
Connor M <63883267+Connor406@users.noreply.github.com>
2020-09-23 18:34:17 -0400

    Updates

---
+++ precache-manifest.70d7af914ab782179ebe73196f54bc65.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a119dd399e058a6ba51c63023ee4da",
+    "url": "/wedding-scrinzi/index.html"
+  },
+  {
+    "revision": "2e0485f60ccd48488fcc",
+    "url": "/wedding-scrinzi/static/css/main.dd50044c.chunk.css"
+  },
+  {
+    "revision": "c15c9e6c28ed37b2c7c3",
+    "url": "/wedding-scrinzi/static/js/2.b86b4907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wedding-scrinzi/static/js/2.b86b4907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e0485f60ccd48488fcc",
+    "url": "/wedding-scrinzi/static/js/main.922b9640.chunk.js"
+  },
+  {
+    "revision": "1d9f3bffc663a711fb51",
+    "url": "/wedding-scrinzi/static/js/runtime-main.3298a269.js"
+  },
+  {
+    "revision": "8a71d4a1172731fa8cfe2947e10decc8",
+    "url": "/wedding-scrinzi/static/media/C&B.8a71d4a1.jpg"
+  },
+  {
+    "revision": "9c43ddd580525b0fa03ea92b221a02e1",
+    "url": "/wedding-scrinzi/static/media/EmCapo.9c43ddd5.jpg"
+  },
+  {
+    "revision": "fdd10873f9b2b8ffe414ef939a36cf23",
+    "url": "/wedding-scrinzi/static/media/TuesdayNight-Regular.fdd10873.otf"
+  },
+  {
+    "revision": "17e3ed5db5c907d097687754232a10ad",
+    "url": "/wedding-scrinzi/static/media/pic.17e3ed5d.jpg"
+  },
+  {
+    "revision": "6483cf47f3ae3033f7eab92d7f8b264a",
+    "url": "/wedding-scrinzi/static/media/tgt.6483cf47.jpg"
+  }
+]);
